31ca0d5c428c98132f8b5d41c33fcdacc80ac419
kidnab <90466472+kidnab@users.noreply.github.com>
2023-11-14 02:20:27 +0000

    entry, index, check search

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -7,6 +7,10 @@ from markdown2 import Markdown
 def convert(title):
     content = util.get_entry(title)
     markdowner = Markdown()
+    if content == None: 
+        content = util.get_entry(title.capitalize())
+    if content == None: 
+        content = util.get_entry(title.upper())
     if content == None:
         return None
     else:
@@ -16,7 +20,7 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
-
+ 
 def entry(request, title):
     html_content = convert(title)
     if html_content == None:
@@ -24,7 +28,26 @@ def entry(request, title):
             "message": "This entry does not exist."
            })
     else:
-        return render(request, "encyclopedia/entry.html",
+        return render(request, "encyclopedia/entry.html", {
             "title": title,
-            "content" : html_content,
-        )
+            "content": html_content
+        })
+    
+def search(request):
+    if request.method == "POST":
+        entry_search = request.POST['q']
+        html_content = convert(entry_search)
+        if html_content is not None:
+            return render(request, "encyclopedia/entry.html", {
+                "title": entry_search,
+                "content": html_content
+            })
+        else: 
+            entries = util.list_entries()
+            search_results = []
+            for entry in entries:
+                if entry_search.lower() in entry.lower():
+                    search_results.append(entry)
+            return render(request, "encyclopedia/search.html", {
+                "search results": search_results
+            })
